9a2409e1b204ac59a92362e38225fa0a9125d275
Gumbo_KIM <dae4805@naver.com>
2021-03-25 15:43:48 +0900

    SPRING MVC

--- JAVA/SPRING/MiniProjectJava/WebContent/WEB-INF/views/include/bottom_info.jsp
+++ JAVA/SPRING/MiniProjectJava/WebContent/WEB-INF/views/include/bottom_info.jsp
@@ -1,7 +1,5 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-    <%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %>
-<c:url var='root' value='/'/>
 <div class="container-fluid bg-dark text-white" style="margin-top:50px;padding-top:30px;padding-bottom:30px">
 	<div class="container">
 		<p>http://www.softcampus.co.kr</p>
--- JAVA/SPRING/MiniProjectJava/src/controller/UserController.java
+++ JAVA/SPRING/MiniProjectJava/src/controller/UserController.java
@@ -1,21 +1,67 @@
 package controller;
 
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import beans.UserBean;
+import service.UserService;
+import validator.UserValidator;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
 	
+	@Autowired
+	private UserService userService;
+	
+	@Resource(name = "loginUserBean")
+	private UserBean loginUserBean;
+	
 	@GetMapping("/login")
-	public String login() {
+	public String login(@ModelAttribute("tempLoginUserBean") UserBean tempLoginUserBean) {
 		return "user/login";
 	}
 	
+	@GetMapping("/login_pro")
+	public String login_pro(@ModelAttribute("tempLoginUserBean") UserBean tempLoginUserBean, BindingResult result) {
+		
+		if(result.hasErrors()) {
+			return "user/login";
+		}
+		
+		userService.getLoginUserInfo(tempLoginUserBean);
+		
+		return "user/login_success";
+		
+	}
+	
+	
+	
 	@GetMapping("/join")
-	public String join() {
+	public String join(@ModelAttribute("joinUserBean") UserBean joinuserBean) {
 		return "user/join";
+	}
+	
+	@PostMapping("/join_pro")
+	public String join_pro(@Valid @ModelAttribute("joinUserBean") UserBean joinUserBean, BindingResult result) {
+		if(result.hasErrors()) {
+			return "user/join";
+		}
+		
+		userService.addUserInfo(joinUserBean);
+		
+		
+		return "user/join_success";
 	}
 	
 	@GetMapping("/modify")
@@ -26,6 +72,13 @@ public class UserController {
 	@GetMapping("/logout")
 	public String logout() {
 		return "user/logout";
+	}	
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		UserValidator validator1 = new UserValidator();
+		binder.addValidators(validator1);
 	}
+
 	
 }
