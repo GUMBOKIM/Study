ac28a74730c4caa143f0ab5089724c2e7be4fa81
DESKTOP-Q7SSSEG\dae48 <dae4805@naver.com>
2021-08-17 13:15:09 +0900

    Mongoose

---
+++ BackEnd/NodeJS/Chapter7/models/index.js
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Comment = require('./comment');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config')[env];
+const db = {};
+
+const sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = User;
+db.Comment = Comment;
+
+// DB랑 연결해준다
+User.init(sequelize);
+Comment.init(sequelize);
+
+//
+User.associate(db);
+Comment.associate(db);
+
+module.exports = db;
