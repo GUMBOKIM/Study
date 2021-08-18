68e7553793ec492b63becf2f8f9bb6df0e894d5e
Daehee Kim <dae4805@naver.com>
2021-08-18 14:17:16 +0900

    Test

--- BackEnd/NodeJS/Chapter11/controllers/user.test.js
+++ BackEnd/NodeJS/Chapter11/controllers/user.test.js
@@ -1,23 +1,18 @@
 jest.mock('../models/user');
-const User = require('../models/user');
-const { addFollowing } = require('./user');
+const { addFollowing } = require('../controllers/user');
 
 describe('addFollowing', () => {
-  const req = {
-    user: { id: 1 },
-    params: { id: 2 },
-  };
   const res = {
     send: jest.fn(),
+    status: jest.fn(() => res),
   };
   const next = jest.fn();
 
   test('사용자를 찾아 팔로잉을 추가하고 success를 응답해야 함', async () => {
-    User.findOne.mockReturnValue(Promise.resolve({
-      addFollowing(id) {
-        return Promise.resolve(true);
-      }
-    }));
+    const req = {
+      user: { id: 1 },
+      params: { id: 2 },
+    };
     await addFollowing(req, res, next);
     expect(res.send).toBeCalledWith('success');
   });
