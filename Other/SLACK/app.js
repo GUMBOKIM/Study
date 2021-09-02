f686967c6fad19cf8dee4af747466e6b7bb18ae8
Daehee Kim <dae4805@naver.com>
2021-09-02 17:49:12 +0900

    Test SLACK Webhook

---
+++ Other/SLACK/app.js
@@ -0,0 +1,23 @@
+const Slack = require("slack-node");
+
+const WebHookURL =
+  "https://hooks.slack.com/services/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";
+const userName = "dae4414805";
+
+const slack = new Slack();
+
+slack.setWebhook(WebHookURL);
+
+const send = async (message) => {
+  slack.webhook(
+    {
+      channel: `@${userName}`,
+      text: "짬뽕",
+    },
+    function (err, response) {
+      console.log(response);
+    }
+  );
+};
+
+send();
