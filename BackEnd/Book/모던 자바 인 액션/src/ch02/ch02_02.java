9b662669d36ea73a6ee5cb671d0b7ade38969d00
gumbo <dae4805@naver.com>
2022-02-09 23:23:33 +0900

    20220208 - TIL 책 - 모던 자바 인 액션

--- BackEnd/Book/모던 자바 인 액션/src/ch02/ch02_02.java
+++ BackEnd/Book/모던 자바 인 액션/src/ch02/ch02_02.java
@@ -10,18 +10,4 @@ import java.util.function.Supplier;
 
 import static common.Color.GREEN;
 
-// step 4
-public class ch02_02 implements Supplier {
-
-    public static List<Apple> filterApples(List<Apple> inventory, ApplePredicate p) {
-        List<Apple> result = new ArrayList<>();
-        for (Apple apple : inventory) {
-            if (p.test(apple)) {
-                result.add(apple);
-            }
-        }
-        return result;
-    }
-
-}
 
