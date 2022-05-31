eaed92d0b6216fc1c0f67fc754862c90b5f5843c
Daehee Kim <daehee_kim2@tmax.co.kr>
2022-05-31 16:32:40 +0900

    커밋

---
+++ Algorithm/Programmers/src/highScoreKit/sort/BigNumber.java
@@ -0,0 +1,20 @@
+package highScoreKit.sort;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class BigNumber {
+
+
+    static void so(int[][] costs, int xcost, int ycost){
+        System.out.println("Arrays.toString(costs) = " + Arrays.toString(costs));
+    }
+
+
+
+
+    public static void main(String[] args) {
+        String result = solution(new int[]{99998, 9});
+        System.out.println("result = " + result);
+    }
+}
