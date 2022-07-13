88f0f08a25725a0118916a2a0ee0316bcd908f16
daeheekim <dae4805@naver.com>
2022-07-13 19:44:31 +0900

    웹사이트 성능 최적화 4 1. Layout Shift 피하기 위한 LazyLoad 사용

---
+++ FrontEnd/Performance-Optimize/lecture-4-master/src/components/PhotoItem.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useDispatch } from 'react-redux';
+import LazyLoad from 'react-lazyload';
+import { showModal } from '../redux/imageModal';
+
+function PhotoItem({ photo: { urls, alt } }) {
+  const dispatch = useDispatch();
+
+  const openModal = () => {
+    dispatch(showModal({ src: urls.full, alt }));
+  };
+
+  return (
+    <ImageWrap>
+      <LazyLoad offset={500}>
+      <Image src={urls.small} alt={alt} onClick={openModal} />
+      </LazyLoad>
+    </ImageWrap>
+  );
+}
+
+const ImageWrap = styled.div`
+  width: 100%;
+  padding-bottom: 56.25%;
+  position: relative;
+`;
+
+const Image = styled.img`
+  cursor: pointer;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+`;
+
+export default PhotoItem;
