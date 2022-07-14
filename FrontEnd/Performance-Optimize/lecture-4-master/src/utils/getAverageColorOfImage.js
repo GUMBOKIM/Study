81e216b1cb1e3b8fe382aabd810cec9fcdb1e0bf
daeheekim <dae4805@naver.com>
2022-07-14 15:53:15 +0900

    웹사이트 성능 최적화 4 4. 데이터 캐싱 처리

--- FrontEnd/Performance-Optimize/lecture-4-master/src/utils/getAverageColorOfImage.js
+++ FrontEnd/Performance-Optimize/lecture-4-master/src/utils/getAverageColorOfImage.js
@@ -1,4 +1,20 @@
-export function getAverageColorOfImage(imgElement) {
+const memoize = (fn) => {
+  const cache = {};
+
+  return function (...args) {
+    if(args.length !== 1){
+      return fn(...args);
+    }
+    if(cache.hasOwnProperty(args)) {
+      return cache[args];
+    }
+    const result = fn(...args);
+    cache[args] = result;
+    return result;
+  }
+}
+
+function getAverageColorOfImageFn(imgElement) {
   const canvas = document.createElement('canvas');
   const context = canvas.getContext && canvas.getContext('2d');
   const averageColor = {
@@ -12,9 +28,9 @@ export function getAverageColorOfImage(imgElement) {
   }
 
   const width = (canvas.width =
-    imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width);
+      imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width);
   const height = (canvas.height =
-    imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height);
+      imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height);
 
   context.drawImage(imgElement, 0, 0);
 
@@ -34,3 +50,51 @@ export function getAverageColorOfImage(imgElement) {
 
   return averageColor;
 }
+
+export const getAverageColorOfImage = memoize(getAverageColorOfImageFn);
+
+
+// const cache = {};
+//
+// export function getAverageColorOfImage(imgElement) {
+//   if(cache.hasOwnProperty(imgElement.src)) {
+//     return cache[imageElement.src];
+//   }
+//
+//   const canvas = document.createElement('canvas');
+//   const context = canvas.getContext && canvas.getContext('2d');
+//   const averageColor = {
+//     r: 0,
+//     g: 0,
+//     b: 0,
+//   };
+//
+//   if (!context) {
+//     return averageColor;
+//   }
+//
+//   const width = (canvas.width =
+//     imgElement.naturalWidth || imgElement.offsetWidth || imgElement.width);
+//   const height = (canvas.height =
+//     imgElement.naturalHeight || imgElement.offsetHeight || imgElement.height);
+//
+//   context.drawImage(imgElement, 0, 0);
+//
+//   const imageData = context.getImageData(0, 0, width, height).data;
+//   const length = imageData.length;
+//
+//   for (let i = 0; i < length; i += 4) {
+//     averageColor.r += imageData[i];
+//     averageColor.g += imageData[i + 1];
+//     averageColor.b += imageData[i + 2];
+//   }
+//
+//   const count = length / 4;
+//   averageColor.r = ~~(averageColor.r / count); // ~~ => convert to int
+//   averageColor.g = ~~(averageColor.g / count);
+//   averageColor.b = ~~(averageColor.b / count);
+//
+//   cache[imgElement.src] = averageColor;
+//
+//   return averageColor;
+// }
