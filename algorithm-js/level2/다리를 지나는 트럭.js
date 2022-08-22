00f732a77bc30d0b6d8e84205d2ec6fa778b3fe4
daeheekim <dae4805@naver.com>
2022-08-22 14:46:09 +0900

    알고리즘

--- algorithm-js/level2/다리를 지나는 트럭.js
+++ algorithm-js/level2/다리를 지나는 트럭.js
@@ -2,14 +2,31 @@ function solution(bridge_length, weight, truck_weights) {
     truck_weights.reverse();
     let answer = 0;
 
+    let nowTruck = 0;
+    let bridgeWeight = 0;
     let bridge = new Array(bridge_length).fill(0);
 
-    let checkBridge = () => {
-        bridge.reduce()
+    const move = input => {
+        let last = bridge[bridge_length - 1];
+        bridge = [input, ...bridge.slice(0, bridge_length - 1)]
+        bridgeWeight -= last;
     }
 
+    const addCheck = input =>  bridgeWeight + input <= weight;
 
-    return answer;
+    while(nowTruck < truck_weights.length){
+        let weight = truck_weights[nowTruck];
+        if(addCheck(weight)) {
+            move(weight);
+            bridgeWeight += weight;
+            nowTruck ++;
+        } else {
+            move(0);
+        }
+        if(bridgeWeight != 0) answer ++;
+    }
+
+    return answer + bridge_length;
 }
 
-console.log(2, 10, [7, 4, 5, 6])
+console.log(solution(2, 10, [7, 4, 5, 6]))
