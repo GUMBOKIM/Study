08c1c71362c2ea6c2c19a8e224d36e9c71efe539
daeheekim <dae4805@naver.com>
2022-11-12 00:14:38 +0900

    tailwind practice

--- FrontEnd/Nextjs/carrot-market/pages/index.tsx
+++ FrontEnd/Nextjs/carrot-market/pages/index.tsx
@@ -2,8 +2,8 @@ import type {NextPage} from "next";
 
 const Home: NextPage = () => {
     return (
-        <div className="bg-slate-400 py-20 px-20 flex flex-col space-y-5">
-            <div className="bg-white p-5 rounded-2xl shadow-xl">
+        <div className="bg-slate-400 py-20 px-20 flex flex-col  space-y-5 min-h-screen">
+            <div className="bg-white p-5 rounded-2xl shadow-xl flex flex-col justify-center">
                 <span className="font-semibold text-3xl">Select Item</span>
                 <div className="flex justify-between my-2">
                     <span className="text-gray-500">Grey Chair</span>
@@ -17,7 +17,9 @@ const Home: NextPage = () => {
                     <span>Total</span>
                     <span className="font-semibold">$99</span>
                 </div>
-                <div className="mt-5 mx-auto bg-blue-500 text-white p-5 text-center rounded-2xl w-1/2">Checkout</div>
+                <button
+                    className="mt-5 mx-auto bg-blue-500 text-white p-5 text-center rounded-2xl w-1/2 hover:bg-teal-500 active:bg-yellow-500 focus:bg-red-500">Checkout
+                </button>
             </div>
             <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
                 <div className="bg-blue-500 p-6 pb-14">
@@ -41,8 +43,40 @@ const Home: NextPage = () => {
                     </div>
                 </div>
             </div>
-            <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-            <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+            <div className="bg-white p-10 rounded-2xl shadow-xl">
+                <div className="flex mb-5 justify-between items-center">
+                    <span>⬅</span>
+                    <div className="space-x-3">
+                        <span>★ 4.9</span>
+                        <span className="shadow-xl p-2 rounded-md ">♥️</span>
+                    </div>
+                </div>
+                <div className="bg-zinc-400 h-72 mb-5"/>
+                <div className="flex flex-col">
+                    <span className="font-medium text-xl">Swoon Lounge</span>
+                    <span className="text-xs text-gray-500">Chair</span>
+                    <div className="mt-3 mb-5 flex justify-between items-center">
+                        <div className="space-x-2">
+                            <button className="w-5 h-5 rounded-full bg-yellow-500"/>
+                            <button className="w-5 h-5 rounded-full bg-indigo-500"/>
+                            <button className="w-5 h-5 rounded-full bg-teal-500"/>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <button
+                                className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500">-
+                            </button>
+                            <span>1</span>
+                            <button
+                                className="rounded-lg bg-blue-200 flex justify-center items-center aspect-square w-8 font-medium text-xl text-gray-500">+
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex justify-between items-center">
+                    <span className="font-medium text-2xl">$450</span>
+                    <button className="bg-blue-500 py-2 px-8 text-center text-white rounded-xl">Add to cart</button>
+                </div>
+            </div>
         </div>
     )
 };
