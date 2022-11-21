39603203cffb1fbc18be45d840859bc777827583
daeheekim <dae4805@naver.com>
2022-11-21 19:29:02 +0900

    login

--- FrontEnd/Nextjs/carrot-market/libs/server/withHandler.ts
+++ FrontEnd/Nextjs/carrot-market/libs/server/withHandler.ts
@@ -1,18 +1,34 @@
 import {NextApiRequest, NextApiResponse} from "next";
+import withHandler, {ResponseType} from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
-export default function withHandler(
-    method: "GET" | "POST" | "DELETE",
-    fn: (req: NextApiRequest, res: NextApiResponse) => void
+async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<ResponseType>
 ) {
-    return async function (req: NextApiRequest, res: NextApiResponse) {
-        if (req.method !== method) {
-            return res.status(405).end();
-        }
-        try {
-            await fn(req, res);
-        } catch (error) {
-            console.log(error);
-            return res.status(500).json({error});
-        }
-    };
+    const {phone, email} = req.body;
+    const user = phone ? {phone: +phone} : email ? {email} : null;
+    if (!user) return res.status(400).json({ok: false});
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
+    const token = await client.token.create({
+        data: {
+            payload,
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...user,
+                    },
+                    create: {
+                        name: "Anonymous",
+                        ...user,
+                    },
+                },
+            },
+        },
+    });
+    return res.json({
+        ok: true,
+    });
 }
+
+export default withHandler("POST", handler);
